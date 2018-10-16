a0d8cc4a3ee2c0bcd28ab11d72552a71b868acde
suusan2go <ksuzuki180@gmail.com>
2018-10-16 23:42:36 +0900

    デザインの骨組みを作る

--- src/pages/Layout/MainLayout.tsx
+++ src/pages/Layout/MainLayout.tsx
@@ -1,10 +1,42 @@
 import * as React from 'react';
 
-const MainLayout: React.SFC<any> = (props: any) => (
-  <div>
-    <h1>Main</h1>
-    {props.children}
-  </div>
-);
+import { withStyles } from '@material-ui/core/styles';
 
-export default MainLayout;
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const MainLayout: React.SFC<any> = (props: any) => {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            WorkSheet
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <main>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+const styles = {
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  root: {
+    flexGrow: 1
+  }
+};
+
+export default withStyles(styles)(MainLayout);
--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
+
+import { Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Top</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-          <Link to="/projects">projects</Link>
-        </p>
-      </div>
+      <Grid container={true} spacing={24} justify={'center'}>
+        <Grid item={true} xs={11} sm={10} md={10} lg={10}>
+          <div className="App">
+            <p className="App-intro">
+              To get started, edit <code>src/App.tsx</code> and save to reload.
+              <Link to="/projects">projects</Link>
+            </p>
+          </div>
+        </Grid>
+      </Grid>
     );
   }
 }
